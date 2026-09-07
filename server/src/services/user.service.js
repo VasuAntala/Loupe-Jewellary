874d9b4fd777a7c62991eb869579efadc880cea7
Vasu Antala <155867803+VasuAntala@users.noreply.github.com>
2026-09-07 10:43:05 +0530

    changes Backend name to server

--- server/src/services/user.service.js
+++ server/src/services/user.service.js
@@ -1,4 +1,4 @@
-const User = require("../models/user.model");
+const User = require("../models/user.model.js");
 const bcrypt = require('bcrypt');
 const jwtProvider = require("../config/jwtProvider.js");
 
